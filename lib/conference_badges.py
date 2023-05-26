a1c2d4a5941e8fde1dfa5217f1b8866200abd9ec
Zoe Guidry <oyezoey@gmail.com>
2023-05-26 14:12:31 -0500

    lab completed.

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,22 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
 def assign_rooms(names):
-    return None
+    return [f"Hello, {names[num]}! You'll be assigned to room {num+1}!" for num in range(len(names))]
+    # i = 0
+    # room_strings = []
+    
+    # while i < len(names):
+    #     room_strings.append(f"Hello, {names[i]}! You'll be assigned to room {i+1}!")
+    #     i += 1
+    # return room_strings
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+    for assignment in assign_rooms(names):
+        print(assignment)
+    # return None
